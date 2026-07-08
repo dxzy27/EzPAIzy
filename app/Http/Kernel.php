9cc8ea1e93eedef03376f4290ef08b179aba3cbc
dxzy27 <danialhero434@gmail.com>
2026-07-08 21:57:48 +0800

    made changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\EnsureNotSuspended::class,
+            \App\Http\Middleware\PreventBackHistory::class,
         ],
 
         'api' => [
