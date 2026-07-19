d17ce0929cd0516b8eba17f656c3f638bb7e97fe
dxzy27 <danialhero434@gmail.com>
2026-07-19 20:11:13 +0800

    changes made

--- resources/views/student/quizzes.blade.php
+++ resources/views/student/quizzes.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container-fluid px-4 py-5">
+<div class="container py-5">
     <div class="d-flex justify-content-between align-items-center mb-5">
         <div class="d-flex align-items-center gap-3">
             <a href="{{ route('student.dashboard') }}" class="btn btn-outline-secondary btn-sm rounded-circle p-2 d-inline-flex align-items-center justify-content-center" style="width: 36px; height: 36px;" title="Back">
